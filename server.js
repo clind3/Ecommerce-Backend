cce265264652a09aef7f87ac707fa0d5b2984a13
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-08-31 17:16:00 -0400

    add sequelize connection to database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('sequelize');
+const { sequelize } = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +8,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
+sequelize.sync({force: false}).then(()=> {
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+});
+  
